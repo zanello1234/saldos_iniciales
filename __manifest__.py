c9ce8c060e5f7569eb2a7720003160caef7caa60
Martin Zanello <91170739+zanello1234@users.noreply.github.com>
2025-07-11 13:28:03 -0300

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,25 @@
+{
+    "name": "Saldos Iniciales",
+    "summary": """
+        Importación de Saldos por Cobrar y Pagar
+        """,
+    "description": """
+        Módulo para importar archivos CSV con saldos por cobrar o pagar en Odoo 17 Enterprise Edition.
+        - Permite cargar archivos CSV con información de facturas de proveedores o clientes.
+        - Funciona con el monto total de cada factura, sin detalles de IVA.
+        - Compatible con formato de archivo de AFIP.
+        - Soporte para múltiples monedas (DOL -> USD, PES -> ARS).
+        - Actualización automática de tipos de cambio.
+    """,
+    "category": "Accounting",
+    "version": "18.0.1.2.0",
+    "depends": ["base", "account", "l10n_ar", "mail"],
+    "data": [
+        "security/ir.model.access.csv",
+        "account_view.xml",
+    ],
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
