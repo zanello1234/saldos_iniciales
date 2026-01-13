83f63f51cc15e1ad08010ef481995a52c9e6e120
Martin Zanello <91170739+zanello1234@users.noreply.github.com>
2026-01-13 18:00:37 -0300

    Delete saldos_iniciales directory

--- saldos_iniciales/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-from . import account_iva_file
-from . import account_move
-from . import res_partner
-from . import res_currency_rate
